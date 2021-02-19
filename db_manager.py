c25ee5aabd3c1967a489691f10bae8deafa5421d
Afnan Wajdi <73751619+penejedi@users.noreply.github.com>
2021-02-20 02:32:33 +0800

    Update db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -1,40 +1,30 @@
 import mysql.connector
-from hashlib import sha256
-import sys
 
 db = mysql.connector.connect(
     host="localhost",
-    user="youruser",
-    password='yourpassword'
+    user="root",
+    password='Hanafi2096!'
 )
 
-mycursor = db.cursor()
-mycursor.execute('USE password')
-
-
-def master():
-    master_password = input('Enter master password: ')
-
-    try:
-        web = 'master'
-        name = 'master'
-        new_master_password = sha256((master_password).encode('utf-8')).hexdigest()
-        save(web, name, new_master_password)
-        print('Master password created!')
-    except mysql.connector.errors.IntegrityError:
-        hash_input = sha256((master_password).encode('utf-8')).hexdigest()
-        mycursor.execute('SELECT password FROM password_data WHERE website="master" AND username="master"')
-        for i in mycursor:
-            global master_password_hash
-            master_password_hash = i[0]
-        if hash_input == master_password_hash:
-            print('Acces Granted!')
-        else:
-            print('Access Denied!')
-            sys.exit()
+cursor = db.cursor()
+cursor.execute('CREATE DATABASE IF NOT EXISTS password_manager')
+cursor.execute('USE password_manager')
+cursor.execute('CREATE TABLE IF NOT EXISTS password_data ('
+               '    website VARCHAR(50) NOT NULL,'
+               '    username VARCHAR(50) NOT NULL,'
+               '    password VARCHAR(100) NOT NULL,'
+               'PRIMARY KEY (website, username))')
 
 
 def save(website, username, password):
-    mycursor.execute('INSERT INTO password_data (website, username, password) VALUES (%s,%s,%s)',
-                     (website, username, password))
+    cursor.execute('INSERT INTO password_data (website, username, password) VALUES (%s,%s,%s)',
+                   (website, username, password))
     db.commit()
+
+
+def retrieve(website, username):
+    cursor.execute('SELECT password FROM password_data WHERE website=(%s) AND username=(%s)',
+                   (website, username))
+    password = cursor.fetchall()
+    if len(password) == 1:
+        return password[0][0]
