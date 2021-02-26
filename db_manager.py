3743edadf3dbe959427ef5bfc873097fa30260a5
Afnan Wajdi <73751619+penejedi@users.noreply.github.com>
2021-02-26 22:49:24 +0800

    Update db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -12,7 +12,7 @@ cursor.execute('USE password_manager')
 cursor.execute('CREATE TABLE IF NOT EXISTS password_data ('
                '    website VARCHAR(50) NOT NULL,'
                '    username VARCHAR(50) NOT NULL,'
-               '    password VARCHAR(100) NOT NULL,'
+               '    password VARCHAR(200) NOT NULL,'
                'PRIMARY KEY (website, username))')
 
 
